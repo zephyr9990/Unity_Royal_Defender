160a3465f615ec2449cc76af8fe8ff62988ef86d
Rodel Aragon <34167795+zephyr9990@users.noreply.github.com>
2020-01-10 23:20:14 -0800

    Added and completed playermovement script. Added additional folders.

---
+++ Royal Defender/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    private Vector3 movement;
+    private Rigidbody playerRigidbody;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Move(horizontal, vertical);
+    }
+
+    private void Move(float horizontal, float vertical)
+    {
+        movement.Set(horizontal, 0.0f, vertical);
+        movement = movement.normalized * speed * Time.fixedDeltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+
+        // Keeps rotation of last movement direction
+        if (movement.magnitude != 0)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(movement);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+}
