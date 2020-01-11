b47390a0e0905795471c428a81f1010a22669e07
Rodel Aragon <34167795+zephyr9990@users.noreply.github.com>
2020-01-10 22:14:49 -0800

    Added SciFi Warrior to project and created camera follow script

---
+++ Royal Defender/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCameraPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCameraPosition, smoothing * Time.fixedDeltaTime);
+    }
+}
