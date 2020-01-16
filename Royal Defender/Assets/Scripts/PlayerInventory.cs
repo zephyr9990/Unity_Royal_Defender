906f4921b7d58f747ab501872e7d63e8f9f743db
Rodel Aragon <34167795+zephyr9990@users.noreply.github.com>
2020-01-16 12:45:44 -0800

    Added test logic for player to equip and unequip assault rifle.

---
+++ Royal Defender/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    private Animator animator;
+    
+    // TODO delete test value and implement real logic
+    private bool rangedEquipped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+
+        // TODO delete test value and implement real logic
+        rangedEquipped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO Test logic only. Implement real logic
+
+        if (Input.GetButtonDown("SwitchRanged"))
+        {
+            Debug.LogError("Firing");
+            rangedEquipped = !rangedEquipped;
+            animator.SetBool("HasRangedWeapon", rangedEquipped);
+            GameObject gun = gameObject.transform.GetChild(0).transform.GetChild(1).gameObject;
+            gun.SetActive(rangedEquipped); 
+        }
+    }
+}
---
+++ Royal Defender/Assets/Scripts/WeaponInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponInfo : MonoBehaviour
+{
+    public string weaponName;
+    public int damage;
+    public int maxDurability;
+    public GameObject weaponMesh;
+    private int currentDurability;
+
+    WeaponInfo()
+    {
+        currentDurability = maxDurability;
+    }
+
+    public int GetCurrentDurability()
+    {
+        return currentDurability;
+    }
+}
