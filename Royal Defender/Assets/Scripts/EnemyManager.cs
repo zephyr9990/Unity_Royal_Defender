190d07a4fb81bc5e61a537e657985869e7a96820
JasperNat <jnatag01@baker.edu>
2020-01-23 23:37:04 -0500

    Here is my added work for SpawnPoints of the enemies and works as long as you do not beat any of the enemies. There is a problem if you beat a colored boar, and that boar color will not respawn back until the hero health is added!

---
+++ Royal Defender/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,24 @@
+﻿//Jasper Natag-oy
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnTime = 20f;
+    public Transform[] spawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
