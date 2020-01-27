e0c8762fb918b9f9a0f8e758be4246accf449295
jesseJames1691 <jharti02@baker.edu>
2020-01-26 17:09:26 -0700

    Added 3 Health Scripts (CubeHealth, NPCHealth, and PlayerHealth) Also added CubeRotator Script with public speed variable that is increased as the cube's health drops

---
+++ Royal Defender/Assets/Scripts/NPCHealth.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCHealth : MonoBehaviour
+{
+    public int maxHealth = 100;     //Starting health and maximum health for NPC
+    public AudioClip downAudio;    //Down Audio Clip
+
+    Animator anim;                  //NPC animator component
+    AudioSource npcAudio;           //NPC Audio component
+
+    bool isDown;                    //Changed to true when health = 0
+
+    private int currentHealth;      //Current health for NPC
+
+    void Awake()
+    {
+        //Set up references
+        anim = GetComponent<Animator>();
+        npcAudio = GetComponent<AudioSource>();
+
+        // Set up the initial NPC health
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        //Update NPC Health
+        currentHealth -= damageAmount;
+
+        //Check if the NPC is Down
+        if (currentHealth <= 0 && !isDown)
+        {
+            //Incapacitate the NPC
+            Down();
+        }
+    }
+
+    public void RestoreHealth(int restoreAmount)
+    {
+        //Update NPC  Health
+        currentHealth += restoreAmount;
+
+        //Do not let health exceed maximum
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    void Down()
+    {
+        //Set the death flag so this function does not repeat
+        isDown = true;
+
+        //Tell the animator to play the Down animation
+        anim.SetTrigger("Down");
+
+        //Play down audio clip
+        npcAudio.clip = downAudio;
+        npcAudio.Play();
+    }
+}
---
+++ Royal Defender/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;     //Starting health and maximum health for player
+    public Slider healthSlider;     //UI slider component
+    public AudioClip deathAudio;    //Death Audio Clip
+
+
+    Animator anim;                  //Player anim component
+    AudioSource playerAudio;        //Player audio component
+    PlayerMovement PlayerMovement;  //References Player Movement Script
+    PlayerCombat PlayerCombat;      //References Player Combat Script
+
+    bool isDead;                    //Changed to true when currentHealth = 0
+
+    private int currentHealth;      //Current health for the player
+
+    void Awake()
+    {
+        //Set up referenced components
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        PlayerMovement = GetComponent<PlayerMovement>();
+        PlayerCombat = GetComponent<PlayerCombat>();
+   
+
+        // Set up the initial player health
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        //Update Player Health
+        currentHealth -= damageAmount;
+
+        //Update Health Slider
+        healthSlider.value = currentHealth;
+
+        //Check if the player is dead
+        if (currentHealth <= 0 && !isDead)
+        {
+            //Kill the player
+            Death();
+        }
+    }
+
+    public void RestoreHealth(int restoreAmount)
+    {
+        //Update Player Health
+        currentHealth += restoreAmount;
+
+        //Update Health Slider
+        healthSlider.value = currentHealth;
+
+        //Do not let health exceed maximum
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    void Death()
+    {
+        //Set the death flag so this function does not repeat
+        isDead = true;
+
+        //Tell the animator to play the death animation
+        anim.SetTrigger("Die");
+
+        //Play death audio clip
+        playerAudio.clip = deathAudio;
+        playerAudio.Play();
+
+        //Turn off player functions
+        PlayerMovement.enabled = false;
+        PlayerCombat.enabled = false;
+    }
+
+}
