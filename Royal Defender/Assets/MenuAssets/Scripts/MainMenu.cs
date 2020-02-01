e80b89ad5aff8113ec10f5e8eb8849a96941a87f
NightWalker86 <jcherubini86@gmail.com>
2020-01-31 21:03:54 -0500

    Prototype main menu, background music, and new main game music.

---
+++ Royal Defender/Assets/MenuAssets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+
+{
+
+
+    public bool isStart;
+    public bool isQuit;
+
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            SceneManager.LoadScene("CastleTownScene", LoadSceneMode.Single);
+            SceneManager.UnloadScene("MainMenu");
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+}
