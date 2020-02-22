aa5c65b91124293ddbd6aee07e1b9a0e1c38a562
NightWalker86 <jcherubini86@gmail.com>
2020-02-22 01:15:31 -0500

    Redid the MainMenu - Added Skip Prologue option, Controls subscreen, Credits scroll, etc.

---
+++ Royal Defender/Assets/MenuAssets/Scripts/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("PrologueScene", LoadSceneMode.Single);
+    }
+
+    public void SkipPrologue()
+    {
+        SceneManager.LoadScene("CastleTownScene", LoadSceneMode.Single);
+    }
+
+    public void Credits()
+
+    {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
+    public void Quit()
+
+    {
+        Application.Quit();
+    }
+
+}
