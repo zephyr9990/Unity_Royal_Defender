0597986b39f814ead954c4b4e5ac4173457f5545
JasperNat <jnatag01@baker.edu>
2020-02-23 20:05:44 -0500

    Added DecreasingSpawnRate time. Every minute or so you will get a message that enemies spawn faster and 1 second will be taken off every time this message appears.

---
+++ Royal Defender/Assets/Scripts/Managers/DecreasingSpawnRate.cs
@@ -0,0 +1,40 @@
+﻿//Jasper Natag-oy
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecreasingSpawnRate : MonoBehaviour
+{
+    public float spawnRate = 30f;
+    public float spawnRateDropSpeed = 0.001f;
+    public float spawnRateDeceleration = 0.001f;
+    public float minSpawnRate = 1f;
+
+    float lastSpawnTime;
+    float accumulatedTime;
+
+    void OnEnable()
+    {
+        accumulatedTime = 0f;
+        lastSpawnTime = 0f;
+    }
+
+    void Update()
+    {
+        accumulatedTime += Time.deltaTime;
+
+        spawnRateDropSpeed += spawnRateDeceleration * Time.deltaTime;
+        spawnRate = Mathf.Max(spawnRate - Time.deltaTime * spawnRateDropSpeed, minSpawnRate);
+
+        if (accumulatedTime - lastSpawnTime > spawnRate)
+        {
+            lastSpawnTime = accumulatedTime;
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        Debug.Log("Enemies will respawn faster and spawned at" + accumulatedTime);
+    }
+}
